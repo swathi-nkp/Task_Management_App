5ce0ca7a70165086dcf89e315808185fc80c0d4e
Swathi <swathi.pkn@gmail.com>
2026-07-22 13:40:06 +0530

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -24,13 +24,18 @@ const Login = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
-      <form
-        onSubmit={handleLogin}
-        className="bg-white shadow-md rounded-lg p-6 w-full max-w-sm"
-      >
-        <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
+    <>
+  <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
+      <h1 className="text-3xl font-bold text-center text-blue-600 mb-2">
+        Task Management App
+      </h1>
+      <br />
+      <h2 className="text-2xl font-bold text-center mb-6">
+        Login
+      </h2>
 
+      <form onSubmit={handleLogin}>
         <div className="mb-4">
           <label className="block mb-2">Email</label>
           <input
@@ -53,12 +58,22 @@ const Login = () => {
           />
         </div>
 
-        <button type="submit" className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700">
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700"
+        >
           Login
         </button>
       </form>
+
     </div>
+  </div>
+);
+
+export default Login;
+    </>
   );
-}
+
+}  
 
 export default Login;
